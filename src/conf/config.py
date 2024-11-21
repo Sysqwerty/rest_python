ddde6b6559e3f07d8c140d82e949cb1717613fa2
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-11-21 21:24:57 +0200

    add user and JWT token handlers

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,5 +1,8 @@
 class Config:
-    DB_URL = "postgresql+asyncpg://postgres:password@localhost:5432/todo_app"
+    DB_URL = "postgresql+asyncpg://postgres:password@localhost:5432/postgres"
+    JWT_SECRET = "secret_key"  # Секретний ключ для токенів
+    JWT_ALGORITHM = "HS256"  # Алгоритм шифрування токенів
+    JWT_EXPIRATION_SECONDS = 3600  # Час дії токена (1 година)
 
 
 config = Config
