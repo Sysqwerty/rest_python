b40123f77a95a07014d564366c2617d5158a0d93
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-11-22 17:21:21 +0200

    add user_agent_ban_middleware() middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 
+import re
 from ipaddress import ip_address
 from typing import Callable
 
@@ -24,6 +25,8 @@ allowed_ips = [
     ip_address("127.0.0.1")
 ]
 
+user_agent_ban_list = [r"Gecko2", r"Python-urllib"]
+
 
 @app.middleware("http")
 async def limit_access_by_ip(request: Request, call_next: Callable):
@@ -36,6 +39,16 @@ async def limit_access_by_ip(request: Request, call_next: Callable):
         return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"detail": "Not allowed IP address"})
     response = await call_next(request)
 
+    return response
+
+
+@app.middleware("http")
+async def user_agent_ban_middleware(request: Request, call_next: Callable):
+    user_agent = request.headers.get("user-agent")
+    for ban_pattern in user_agent_ban_list:
+        if re.search(ban_pattern, user_agent):
+            return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"detail": "You are banned"})
+    response = await call_next(request)
     return response
 
 
